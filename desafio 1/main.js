bbd092b7c5301498931dd89e3c33aae1927bde72
Nicolas Clos <nicolasclos1@gmail.com>
2022-09-25 19:06:42 -0300

    Subido ya el desafio 1

---
+++ desafio 1/main.js
@@ -0,0 +1,48 @@
+class Usuario{
+    constructor(nombre, apellido, libros, mascotas){
+        this.nombre = nombre,
+        this.apellido = apellido,
+        this.libros = libros,
+        this.mascotas = mascotas
+    };
+
+    getFullName(){
+        console.log(`Su nombre completo es ${this.nombre} ${this.apellido}`)
+    };
+    addMascota(nuevaMascota){
+        this.mascotas.push(nuevaMascota)
+    };
+    countMascotas(){
+        if(this.mascotas == 1){
+            console.log(`Tiene ${this.mascotas.length} mascota`);
+        } else{
+            console.log(`Tiene ${this.mascotas.length} mascotas`);
+        }
+        
+    };
+    addBook(nombre, autor){
+        this.libros.push({nombre: nombre, autor: autor})
+    };
+    getBookNames(){
+        this.libros.map(libro=>console.log(libro.nombre))
+    };
+};
+
+const nuevoUsuario = new Usuario('Nicolas', 'Clos', [{nombre: "LIBRO 1", autor: "AUTOR 1"},{nombre: 'LIBRO 2', autor: 'AUTOR 2'}], [])
+
+console.log(nuevoUsuario)
+
+nuevoUsuario.getFullName();
+
+nuevoUsuario.addMascota("Rene Nicolas");
+
+nuevoUsuario.countMascotas();
+
+nuevoUsuario.addBook('¿Como criar mascotas que son tutores?','Nicolas Clos');
+
+// ES CHISTE NO SE ME ENOJE :)
+
+nuevoUsuario.getBookNames()
+
+
+
