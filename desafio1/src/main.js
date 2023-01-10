d865bdaf65c8794f9fc1352da6cfc7886e21e963
Nicolas Clos <nicolasclos1@gmail.com>
2023-01-10 11:33:57 -0300

    Desafio 1

--- desafio1/src/main.js
+++ desafio1/src/main.js
@@ -18,11 +18,13 @@ class ProductManager {
             const findCode = this.products.find(product => product.code == code);
             if(findCode != undefined || findCode != null){
                 console.log('El código debe ser único por cada producto')
+            } else if ((title == null || undefined) || (description == null || undefined) || (price == null || undefined) || (thumbnail == null || undefined) || (code == null || undefined) || (stock == null || undefined)){
+                console.log('Todos los campos son obligatorios')
             } else {
                 this.products.push(newProduct);
             }
         }
-
+        codeValidator()
     }
 
     getProductById(id){        
@@ -45,9 +47,6 @@ const manager = new ProductManager();
 
 manager.addProduct('iPhone 13', '256GB', 999, 'apple.com/iphone13.png', 12, 213)
 
-/*manager.addProduct('iPhone 14 PRO MAX', '256GB', 1299, 'apple.com/iphone14promax.png', 410, 23);*/
+manager.addProduct('iPhone 14 PRO MAX', '256GB', 1299, 'apple.com/iphone14promax.png', 23);
 
 manager.getProducts();
-
-
-
